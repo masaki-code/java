296b3779242a217b24527ad6005f4948aa291a1e
masaki <masaki@masaki-blog.net>
2020-03-01 00:25:26 +0900

    modified

--- sample/src/main/java/net/masaki_blog/sample/Sample.java
+++ sample/src/main/java/net/masaki_blog/sample/Sample.java
@@ -1,7 +1,10 @@
 package net.masaki_blog.sample;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Sample {
 
@@ -9,12 +12,29 @@ public class Sample {
 //        return list.stream().map(s -> s + ":test").collect(Collectors.toList());
 //    }
 
+//    public List<String> sample(List<String> list) {
+//
+//        List<String> ret = new ArrayList<>();
+//        for (String s : list) {
+//            ret.add(s + ":test");
+//        }
+//        return ret;
+//    }
+
     public List<String> sample(List<String> list) {
 
-        List<String> ret = new ArrayList<>();
-        for (String s : list) {
-            ret.add(s + ":test");
-        }
-        return ret;
+        Stream<String> originStream = list.stream();
+        Function<String, String> mapper = new Function<String, String>() {
+
+            @Override
+            public String apply(String t) {
+                return t + ":test";
+            }
+        };
+
+        Stream<String> mapedStream = originStream.map(mapper);
+        Collector<String, ?, List<String>> c = Collectors.toList();
+        return mapedStream.collect(c);
     }
+
 }
