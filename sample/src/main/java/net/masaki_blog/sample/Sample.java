e5e62126297c5ce142e6bbdd94cad8d58f045234
masaki <masaki@masaki-blog.net>
2020-03-01 00:17:32 +0900

    modified

--- sample/src/main/java/net/masaki_blog/sample/Sample.java
+++ sample/src/main/java/net/masaki_blog/sample/Sample.java
@@ -1,11 +1,20 @@
 package net.masaki_blog.sample;
 
+import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class Sample {
 
-	public List<String> sample(List<String> list) {
-		return list.stream().map(s -> s + ":test").collect(Collectors.toList());
-	}
+//    public List<String> sample(List<String> list) {
+//        return list.stream().map(s -> s + ":test").collect(Collectors.toList());
+//    }
+
+    public List<String> sample(List<String> list) {
+
+        List<String> ret = new ArrayList<>();
+        for (String s : list) {
+            ret.add(s + ":test");
+        }
+        return ret;
+    }
 }
