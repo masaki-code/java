4771d302d60df1a236b582ff16d56b7d64c40bae
masaki <masaki@masaki-blog.net>
2020-03-01 21:58:14 +0900

    modified

--- sample/src/main/java/net/masaki_blog/sample/Sample.java
+++ sample/src/main/java/net/masaki_blog/sample/Sample.java
@@ -7,6 +7,8 @@ import java.util.stream.Collectors;
 
 public class Sample {
 
+    private static final Collector<String, ?, List<String>> TO_LIST = Collectors.toList();
+
 // 本当はこれで良い。
 //    public List<String> sample(List<String> list) {
 //        return list.stream().map(s -> s + ":test").collect(Collectors.toList());
@@ -16,14 +18,12 @@ public class Sample {
      * java8のメソッド（stream api）は使えるが、java8の構文（ラムダ式など）は使えない状態でのメソッド
      */
     public List<String> sample(List<String> list) {
-        Collector<String, ?, List<String>> toList = Collectors.toList();
-
         return list.stream().map(new Function<String, String>() {
             @Override
             public String apply(String s) {
                 return s + ":test";
             }
-        }).collect(toList);
+        }).collect(TO_LIST);
     }
 
 }
