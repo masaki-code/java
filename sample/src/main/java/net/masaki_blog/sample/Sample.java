bc3303eb18b55847cfdbed283eb1c723bc79f6a5
masaki <masaki@masaki-blog.net>
2020-03-02 22:26:20 +0900

    work

--- sample/src/main/java/net/masaki_blog/sample/Sample.java
+++ sample/src/main/java/net/masaki_blog/sample/Sample.java
@@ -1,29 +1,64 @@
 package net.masaki_blog.sample;
 
+import java.util.ArrayList;
 import java.util.List;
-import java.util.function.Function;
-import java.util.stream.Collector;
-import java.util.stream.Collectors;
 
 public class Sample {
-
-    private static final Collector<String, ?, List<String>> TO_LIST = Collectors.toList();
 
 // 本当はこれで良い。
 //    public List<String> sample(List<String> list) {
 //        return list.stream().map(s -> s + ":test").collect(Collectors.toList());
 //    }
 
-    /**
-     * java8のメソッド（stream api）は使えるが、java8の構文（ラムダ式など）は使えない状態でのメソッド
-     */
+//    /**
+//     * java8のメソッド（stream api）は使えるが、java8の構文（ラムダ式など）は使えない状態でのメソッド
+//     */
+//    public List<String> sample(List<String> list) {
+//        return list.stream().map(new Function<String, String>() {
+//            @Override
+//            public String apply(String s) {
+//                return s + ":test";
+//            }
+//        }).collect(TO_LIST);
+//    }
+
+//    public List<String> sample(List<String> list) {
+//        return list.stream().map(this.convert()).collect(Collectors.toList());
+//    }
+
+//    private static final Collector<String, ?, List<String>> TO_LIST = Collectors.toList();
+//
+//    public List<String> sample(List<String> list) {
+//        return list.stream().map(this.convert()).collect(TO_LIST);
+//    }
+//
+//    private Function<String, String> convert() {
+//        return new Function<String, String>() {
+//            @Override
+//            public String apply(String s) {
+//                return s + ":test";
+//            }
+//        };
+//    }
+
+//    public List<String> sample(List<String> list) {
+//        Collector<String, ?, List<String>> toList = Collectors.toList();
+//
+//        return list.stream().map(new Function<String, String>() {
+//            @Override
+//            public String apply(String s) {
+//                return s + ":test";
+//            }
+//        }).collect(toList);
+//    }
     public List<String> sample(List<String> list) {
-        return list.stream().map(new Function<String, String>() {
-            @Override
-            public String apply(String s) {
-                return s + ":test";
-            }
-        }).collect(TO_LIST);
+
+        List<String> ret = new ArrayList<>();
+        for (String s : list) {
+            ret.add(s + ":test");
+        }
+        return ret;
+
     }
 
 }
