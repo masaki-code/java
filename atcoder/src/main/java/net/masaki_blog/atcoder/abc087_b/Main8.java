3685b13665d10243e7e4b8e08b9d7aae56e7ec1c
masaki <masaki@masaki-blog.net>
2020-04-09 21:57:10 +0900

    modified

--- atcoder/src/main/java/net/masaki_blog/atcoder/abc087_b/Main8.java
+++ atcoder/src/main/java/net/masaki_blog/atcoder/abc087_b/Main8.java
@@ -2,7 +2,7 @@ package net.masaki_blog.atcoder.abc087_b;
 
 import java.util.Scanner;
 
-public class Main {
+public class Main8 {
 
     public static void main(String... args) {
         int x, a, b, c;
@@ -13,15 +13,18 @@ public class Main {
             x = sc.nextInt(); // = x
         }
 
-        System.out.println(execute(x / 50, a, b, c));
+        System.out.println(new Main8().count(x / 50, a, b, c));
 
     }
 
-    private static int execute(int x, int a_max, int b_max, int c_max) {
+    private int count(int x, int a_max, int b_max, int c_max) {
+        // x = 10a + 2b + c
+        // x' = 2b + c
+        // => x = 10a + x'
+
         int limit = Math.min(x / 10, a_max);
 
         int count = 0;
-
         for (int a = 0; a <= limit; a++) {
             count += count(x - a * 10, b_max, c_max);
         }
@@ -30,8 +33,20 @@ public class Main {
 
     }
 
-    private static int count(int x, int b_max, int c_max) {
-        return Math.min((x / 2), b_max) - Math.max((x - c_max) / 2, 0) + 1;
+    private int count(int x, int b_max, int c_max) {
+
+        // x' = 2b +c
+        int count = 0;
+
+        int start = Math.max((x - c_max) / 2, 0);
+        int limit = Math.min((x / 2), b_max);
+
+        for (int b = start; b <= limit; b++) {
+            count++;
+
+        }
+
+        return count;
     }
 
 }
