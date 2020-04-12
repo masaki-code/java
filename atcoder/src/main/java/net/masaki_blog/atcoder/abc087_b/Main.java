9206186f233bf64b054a3960cf7cfc5dbcae3492
masaki <masaki@masaki-blog.net>
2020-04-12 19:50:19 +0900

    modified

--- atcoder/src/main/java/net/masaki_blog/atcoder/abc087_b/Main.java
+++ atcoder/src/main/java/net/masaki_blog/atcoder/abc087_b/Main.java
@@ -1,13 +1,12 @@
 package net.masaki_blog.atcoder.abc087_b;
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 
 public class Main {
 
-    public static void main(String... args) throws NumberFormatException, IOException {
+    public static void main(String... args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         PrintWriter pw = new PrintWriter(System.out);
         pw.println(count(
