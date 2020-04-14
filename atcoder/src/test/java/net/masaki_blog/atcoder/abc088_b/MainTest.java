3a3f71f4d97023ea56ffeb6da2a8b9b436053082
masaki <masaki@masaki-blog.net>
2020-04-14 21:38:47 +0900

    modified

--- atcoder/src/test/java/net/masaki_blog/atcoder/abc088_b/MainTest.java
+++ atcoder/src/test/java/net/masaki_blog/atcoder/abc088_b/MainTest.java
@@ -9,6 +9,7 @@ import java.util.List;
 
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.CsvFileSource;
 import org.junit.jupiter.params.provider.MethodSource;
 
 import net.masaki_blog.test.MainTestBase;
@@ -31,4 +32,14 @@ class MainTest extends MainTestBase {
         pattern.add(arguments("4", "20 18 2 18", "18"));
         return pattern;
     }
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "/abc088_b/sample.txt")
+    void testTxt(String n, String a, String expected) throws Exception {
+        in.inputLine(n);
+        in.inputLine(a);
+        Main.main("");
+        assertThat(out.readLine(), is(expected));
+    }
+
 }
---
+++ atcoder/src/test/java/net/masaki_blog/test/DataMakerBase.java
@@ -0,0 +1,48 @@
+package net.masaki_blog.test;
+
+import java.io.InputStream;
+import java.io.PrintStream;
+import java.util.Objects;
+import java.util.Random;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+public class DataMakerBase {
+
+    private static final InputStream SYSTEM_IN = System.in;
+    private static final PrintStream SYSTEM_OUT = System.out;
+
+    protected StandardPrintStream out = StandardPrintStream.has(SYSTEM_OUT);
+    protected StandardInputStream in = StandardInputStream.get();
+
+    private static final Random R = new Random();
+
+    @BeforeEach
+    public void before() {
+        System.setIn(in);
+        System.setOut(out);
+    }
+
+    @AfterEach
+    public void after() {
+        System.setIn(SYSTEM_IN);
+        System.setOut(SYSTEM_OUT);
+    }
+
+    protected String join(int[] params, CharSequence delimiter) {
+        return IntStream.of(params).boxed().map(String::valueOf).collect(Collectors.joining(delimiter));
+    }
+
+    protected <T> String join(T[] params, CharSequence delimiter) {
+        return Stream.of(params).map(Objects::toString).collect(Collectors.joining(delimiter));
+    }
+
+    protected int random(int min, int max, int multiple) {
+        return (R.nextInt(max - min) + min) * multiple;
+    }
+
+}
